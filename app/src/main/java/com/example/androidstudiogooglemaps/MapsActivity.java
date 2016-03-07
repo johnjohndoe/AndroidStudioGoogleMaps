91c9ad8400cb14e8fb6b187e4a881e297e802b62
Tobias Preuss <tobias.preuss@googlemail.com>
2016-03-07 11:43:04 +0100

    Import template from Android Studio 1.5.1.

--- app/src/main/java/com/example/androidstudiogooglemaps/MapsActivity.java
+++ app/src/main/java/com/example/androidstudiogooglemaps/MapsActivity.java
@@ -1,7 +1,7 @@
 package com.example.androidstudiogooglemaps;
 
-import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
